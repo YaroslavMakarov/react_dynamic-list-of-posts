7fae1652d747be2f3885932286505d10200a3f95
Yarik1993 <makarov1603@icloud.com>
2020-05-19 20:03:32 +0300

    Updates

---
+++ precache-manifest.c00bb10052d6ec246f56a195ddc40060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b82d50fcfd6fa1eb125a594fab931b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74199c490cd115e42157",
+    "url": "./static/css/main.2c887c2e.chunk.css"
+  },
+  {
+    "revision": "d21c34f8098573fd4b20",
+    "url": "./static/js/2.20a708a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.20a708a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74199c490cd115e42157",
+    "url": "./static/js/main.c75b7463.chunk.js"
+  },
+  {
+    "revision": "55ecf25ea8d2bbd87954",
+    "url": "./static/js/runtime-main.80c0d849.js"
+  }
+]);
